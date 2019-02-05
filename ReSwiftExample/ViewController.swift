66d124cca5e6adce9e2b63156d57ee99f0b99f9b
Yuki Okubo <y-okubo@nekojarashi.com>
2019-02-05 10:48:28 +0900

    Initial Commit

---
+++ ReSwiftExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ReSwiftExample
+//
+//  Created by Yuki Okubo on 2/5/19.
+//  Copyright © 2019 Nekojarashi Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
